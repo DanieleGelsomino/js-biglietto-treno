9af3372011ee71528f303bb40b7d34868bef15f5
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-04 14:10:46 +0100

    add price ticket without any discounts

--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@
 console.log('JS OK!')
 
 // rihiesta chilometri da percorrere e età del passeggero
-const numeroChilometri = prompt('indica il numero km da percorrere')
-console.log('chilometri da percorrere: ' + numeroChilometri)
+const numeroChilometri = prompt('indica il numero di km da percorrere');
+console.log('km da percorrere: ' + numeroChilometri);
 
-const etàPasseggero = prompt('indica l\'età del passeggero')
-console.log('età passeggero: ' + etàPasseggero)
+const etàPasseggero = prompt('indica l\'età del passeggero');
+console.log('età passeggero: ' + etàPasseggero);
+
+// calcolo prezzo del biglietto in base ai km (0.21€ al km)
+const prezzoTicket = (0.21 * numeroChilometri);
+console.log('prezzo ticket: ' + prezzoTicket);
