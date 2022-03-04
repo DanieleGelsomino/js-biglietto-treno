7c7ae07648be3928cf990e4b8ca101a3f4051887
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-04 15:04:02 +0100

    add price ticket with any discounts exercise complete

--- js/script.js
+++ js/script.js
@@ -10,4 +10,31 @@ console.log('età passeggero: ' + etàPasseggero);
 
 // calcolo prezzo del biglietto in base ai km (0.21€ al km)
 const prezzoTicket = (0.21 * numeroChilometri);
-console.log('prezzo ticket: ' + prezzoTicket);
+console.log('prezzo del biglietto: ' + prezzoTicket.toFixed(2) + '€');
+
+// calcolo prezzo del biglietto con lo sconto del 20% se etàPasseggero è < 18 
+if (etàPasseggero <=18){
+    const prezzoTotale = (prezzoTicket / 100) * 80;
+    const prezzoTotaleFinale = prezzoTotale.toFixed(2);
+    console.log('costo totale del biglietto scontato al 20%: ' + prezzoTotaleFinale + '€')
+    document.getElementById("prezzoTotale").innerText = 
+    `Il costo totale del biglietto per il passeggero con età ${etàPasseggero} anni è di:
+    ${prezzoTotaleFinale} €`;
+
+// calcolo prezzo del biglietto con lo sconto del 40% se etàPasseggero è > 65 
+}else if(etàPasseggero >=65){
+    const prezzoTotale = (prezzoTicket / 100) * 60;
+    const prezzoTotaleFinale = prezzoTotale.toFixed(2);
+    console.log('costo totale del biglietto scontato al 40%: ' + prezzoTotaleFinale + '€')
+    document.getElementById("prezzoTotale").innerText = 
+    `Il costo totale del biglietto per il passeggero con età ${etàPasseggero} anni è di:
+    ${prezzoTotaleFinale} €`;
+
+}else{
+    const prezzoTotale = prezzoTicket;
+    const prezzoTotaleFinale = prezzoTotale.toFixed(2);
+    console.log('costo totale del biglietto: ' + prezzoTotaleFinale + '€')
+    document.getElementById("prezzoTotale").innerText = 
+    `Il costo totale del biglietto per il passeggero con età ${etàPasseggero} anni è di:
+    ${prezzoTotaleFinale} €`;
+}
