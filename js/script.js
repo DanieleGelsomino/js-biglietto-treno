ea5c77eb6e9573ea0c6887fc3bbe5be89a4b8c5a
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-04 13:29:30 +0100

    add first request chilometers add second request age

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 //check console
 console.log('JS OK!')
+
+// rihiesta chilometri da percorrere e età del passeggero
+const numeroChilometri = prompt('indica il numero km da percorrere')
+console.log('chilometri da percorrere: ' + numeroChilometri)
+
+const etàPasseggero = prompt('indica l\'età del passeggero')
+console.log('età passeggero: ' + etàPasseggero)
